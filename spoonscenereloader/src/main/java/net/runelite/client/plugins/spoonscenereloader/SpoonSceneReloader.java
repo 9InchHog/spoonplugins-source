ef5d40121c8f80874b2cb994618366754a7b9c59
9InchHog <whotfspoon@gmail.com>
2022-02-02 11:01:07 -0600

    update scene reloader

--- spoonscenereloader/src/main/java/net/runelite/client/plugins/spoonscenereloader/SpoonSceneReloader.java
+++ spoonscenereloader/src/main/java/net/runelite/client/plugins/spoonscenereloader/SpoonSceneReloader.java
@@ -3,20 +3,28 @@ package net.runelite.client.plugins.spoonscenereloader;
 import com.google.inject.Provides;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
+import net.runelite.api.VarPlayer;
 import net.runelite.api.Varbits;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.GameTick;
 import net.runelite.api.events.VarbitChanged;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.events.ConfigChanged;
 import net.runelite.client.input.KeyListener;
 import net.runelite.client.input.KeyManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.ClientToolbar;
+import net.runelite.client.ui.NavigationButton;
+import net.runelite.client.util.ImageUtil;
 import org.pf4j.Extension;
 
 import javax.inject.Inject;
+import javax.swing.*;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 
 @Extension
 @PluginDescriptor(
@@ -34,42 +42,111 @@ public class SpoonSceneReloader extends Plugin implements KeyListener {
     private KeyManager keyManager;
 
     @Inject
-    private ConfigManager cm;
+    private SpoonSceneReloaderConfig config;
 
     @Inject
-    private SpoonSceneReloaderConfig config;
+    private ClientToolbar clientToolbar;
+
+    private SpoonSceneReloaderPanel panel;
+    private NavigationButton navButton;
+    private boolean buttonAttatched;
+
+    private static final int RAIDS_LOBBY_REGION = 4919;
 
     @Provides
     SpoonSceneReloaderConfig getConfig(ConfigManager configManager) {
-        return (SpoonSceneReloaderConfig)configManager.getConfig(SpoonSceneReloaderConfig.class);
+        return configManager.getConfig(SpoonSceneReloaderConfig.class);
     }
 
     boolean inRaid;
 
     protected void startUp() throws Exception {
-        this.keyManager.registerKeyListener(this);
-        this.cm.unsetConfiguration("raids", "dummy");
+        keyManager.registerKeyListener(this);
+        CreatePanel();
     }
 
     protected void shutDown() throws Exception {
-        this.keyManager.unregisterKeyListener(this);
+        keyManager.unregisterKeyListener(this);
         inRaid = false;
+
+        buttonAttatched = false;
+        clientToolbar.removeNavigation(navButton);
+    }
+
+    public void CreatePanel() {
+        buttonAttatched = false;
+        clientToolbar.removeNavigation(navButton);
+
+        panel = injector.getInstance(SpoonSceneReloaderPanel.class);
+        panel.init();
+
+        final BufferedImage icon = ImageUtil.loadImageResource(getClass(), "instancereloadhelper.png");
+
+        navButton = NavigationButton.builder()
+                .tooltip("Scene Reloader")
+                .icon(icon)
+                .priority(config.panelPriority())
+                .panel(panel)
+                .build();
+    }
+
+    @Subscribe
+    public void onConfigChanged(ConfigChanged event) {
+        if (event.getGroup().equals("scenereloader")) {
+            if (event.getKey().equals("panelPriority")) {
+                CreatePanel();
+            }
+        }
     }
 
     public void keyTyped(KeyEvent e) {}
 
     @Subscribe
-    public void onGameStateChanged(GameStateChanged e) {}
+    public void onGameStateChanged(GameStateChanged e) {
+        if (e.getGameState() == GameState.LOGIN_SCREEN)
+        {
+            SwingUtilities.invokeLater(() ->
+            {
+                clientToolbar.removeNavigation(navButton);
+                buttonAttatched = false;
+            });
+        }
+    }
+
+    @Subscribe
+    public void onGameTick(GameTick event)
+    {
+        boolean isInRaid = client.getVar(Varbits.IN_RAID) == 1;
+        boolean inRaidLobby = (client.getLocalPlayer().getWorldLocation().getRegionID() == RAIDS_LOBBY_REGION);
+        boolean inParty = client.getVar(VarPlayer.IN_RAID_PARTY) != -1;
+        boolean raidsOnly = isInRaid | inRaidLobby | inParty;
+
+        if (config.showPanel() != SpoonSceneReloaderConfig.panelMode.OFF) {
+            if (raidsOnly != buttonAttatched && config.showPanel() == SpoonSceneReloaderConfig.panelMode.RAIDS) {
+                SwingUtilities.invokeLater(() -> {
+                    if (raidsOnly) {
+                        clientToolbar.addNavigation(navButton);
+                    } else {
+                        clientToolbar.removeNavigation(navButton);
+                    }
+                });
+                buttonAttatched = raidsOnly;
+            } else if (config.showPanel() == SpoonSceneReloaderConfig.panelMode.LOGGED_IN) {
+                if (!buttonAttatched) {
+                    SwingUtilities.invokeLater(() -> clientToolbar.addNavigation(navButton));
+                    buttonAttatched = true;
+                }
+            }
+        } else if (buttonAttatched) {
+            SwingUtilities.invokeLater(() -> clientToolbar.removeNavigation(navButton));
+            buttonAttatched = false;
+        }
+    }
 
     public void keyPressed(KeyEvent e) {
         if (e.isControlDown() && e.isShiftDown() && e.getKeyChar() == '\022') {
-            this.clientThread.invoke(new Runnable() {
-                public void run() {
-                    if (SpoonSceneReloader.this.client.getGameState() == GameState.LOGGED_IN)
-                        SpoonSceneReloader.this.cm.setConfiguration("raids", "dummy", String.valueOf(SpoonSceneReloader.this.cm.getConfiguration("raids", "dummy")) + "0");
-                }
-            });
-        } //else if (e.isControlDown() && e.getKeyChar() == '\022') {
+            reload();
+        }
         else if (config.hotkey().matches(e)) {
             if (config.raidsOnly() && inRaid) {
                 reload();
@@ -82,16 +159,15 @@ public class SpoonSceneReloader extends Plugin implements KeyListener {
     public void keyReleased(KeyEvent e) {}
 
     public void reload() {
-        this.clientThread.invoke(new Runnable() {
-            public void run() {
-                if (SpoonSceneReloader.this.client.getGameState() == GameState.LOGGED_IN)
-                    SpoonSceneReloader.this.client.setGameState(GameState.CONNECTION_LOST);
+        clientThread.invoke(() -> {
+            if (client.getGameState() == GameState.LOGGED_IN) {
+                client.setGameState(GameState.CONNECTION_LOST);
             }
         });
     }
 
     @Subscribe
     public void onVarbitChanged(VarbitChanged event) {
-        inRaid = this.client.getVar(Varbits.IN_RAID) == 1;
+        inRaid = client.getVar(Varbits.IN_RAID) == 1;
     }
 }
--- spoonscenereloader/src/main/java/net/runelite/client/plugins/spoonscenereloader/SpoonSceneReloaderConfig.java
+++ spoonscenereloader/src/main/java/net/runelite/client/plugins/spoonscenereloader/SpoonSceneReloaderConfig.java
@@ -1,9 +1,6 @@
 package net.runelite.client.plugins.spoonscenereloader;
 
-import net.runelite.client.config.Config;
-import net.runelite.client.config.ConfigGroup;
-import net.runelite.client.config.ConfigItem;
-import net.runelite.client.config.Keybind;
+import net.runelite.client.config.*;
 
 @ConfigGroup("scenereloader")
 public interface SpoonSceneReloaderConfig extends Config {
@@ -24,4 +21,25 @@ public interface SpoonSceneReloaderConfig extends Config {
             position = 2
     )
     default boolean raidsOnly() { return false; }
+
+    @ConfigItem(
+            keyName = "showPanel",
+            name = "Show Panel",
+            description = "Shows the panel with the button to reload the scene",
+            position = 3
+    )
+    default panelMode showPanel() { return panelMode.LOGGED_IN; }
+
+    @Range(min = 0, max = 10)
+    @ConfigItem(
+            keyName = "panelPriority",
+            name = "Panel Priority",
+            description = "Only reload the scene in CoX",
+            position = 4
+    )
+    default int panelPriority() { return 6; }
+
+    enum panelMode {
+        OFF, RAIDS, LOGGED_IN
+    }
 }
