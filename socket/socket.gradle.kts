4c0dbd76522e7f80a0d44956db590302daeee38d
9InchHog <96740390+9InchHog@users.noreply.github.com>
2022-05-06 11:09:42 -0500

    update

--- socket/socket.gradle.kts
+++ socket/socket.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "1.1.4"
+version = "1.1.5"
 
 project.extra["PluginName"] = "Spoon Socket"
 project.extra["PluginDescription"] = "Contains all Socket plugins from SpoonLite"
