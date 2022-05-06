6809fd821e2be60b343b7ec25da904866a59682e
9InchHog <96740390+9InchHog@users.noreply.github.com>
2022-05-05 19:50:28 -0500

    update

--- spoontileindicators/spoontileindicators.gradle.kts
+++ spoontileindicators/spoontileindicators.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "1.0.3"
+version = "1.0.4"
 
 project.extra["PluginName"] = "Spoon Tile Indicators"
 project.extra["PluginDescription"] = "Additional features for tile indicators"
