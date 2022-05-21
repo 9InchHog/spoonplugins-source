57cd1c734279ca357e4813b6a6f04b9d23f2ae18
9InchHog <96740390+9InchHog@users.noreply.github.com>
2022-05-21 11:35:44 -0500

    Socket ping updated to have images, sounds, and rave

--- socketspoon/src/main/java/net/runelite/client/plugins/socket/plugins/socketping/SocketPingOverlay.java
+++ socketspoon/src/main/java/net/runelite/client/plugins/socket/plugins/socketping/SocketPingOverlay.java
@@ -31,6 +31,7 @@ import net.runelite.client.ui.overlay.OverlayPosition;
 import net.runelite.client.ui.overlay.OverlayPriority;
 import net.runelite.client.ui.overlay.outline.ModelOutlineRenderer;
 import net.runelite.client.util.ImageUtil;
+import net.runelite.client.util.RaveUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -96,12 +97,12 @@ public class SocketPingOverlay extends Overlay {
     private static final long MOUSE_DEBOUNCE_MILLIS = 150L;
 
     private static final BufferedImage WARN_IMAGE;
-
     private static final BufferedImage OMW_IMAGE;
-
     private static final BufferedImage ASSIST_ME_IMAGE;
-
     private static final BufferedImage QUESTION_MARK_IMAGE;
+    private static final BufferedImage PING_IMAGE;
+
+    private final RaveUtils raveUtils;
 
     @Inject
     private Client client;
@@ -109,16 +110,18 @@ public class SocketPingOverlay extends Overlay {
     static {
         colorMapping = new HashMap<>();
         highlightedObjects = ImmutableMap.of(PingType.TARGET, new ConcurrentHashMap<>(), PingType.WARN, new ConcurrentHashMap<>(), PingType.OMW, new ConcurrentHashMap<>(), PingType.ASSIST_ME, new ConcurrentHashMap<>(), PingType.QUESTION_MARK, new ConcurrentHashMap<>());
-        WARN_IMAGE = ImageUtil.loadImageResource(SocketPingOverlay.class, "warn.png");
-        OMW_IMAGE = ImageUtil.loadImageResource(SocketPingOverlay.class, "omw.png");
-        ASSIST_ME_IMAGE = ImageUtil.loadImageResource(SocketPingOverlay.class, "assistme.png");
-        QUESTION_MARK_IMAGE = ImageUtil.loadImageResource(SocketPingOverlay.class, "questionmark.png");
+        WARN_IMAGE = ImageUtil.loadImageResource(SocketPingOverlay.class, "Danger.png");
+        OMW_IMAGE = ImageUtil.loadImageResource(SocketPingOverlay.class, "OMW.png");
+        ASSIST_ME_IMAGE = ImageUtil.loadImageResource(SocketPingOverlay.class, "Assist.png");
+        QUESTION_MARK_IMAGE = ImageUtil.loadImageResource(SocketPingOverlay.class, "Missing.png");
+        PING_IMAGE = ImageUtil.loadImageResource(SocketPingOverlay.class, "Ping.png");
     }
 
     @Inject
-    SocketPingOverlay(SocketPing plugin, SocketPingConfig config) {
+    SocketPingOverlay(SocketPing plugin, SocketPingConfig config, final RaveUtils raveUtils) {
         this.plugin = plugin;
         this.config = config;
+        this.raveUtils = raveUtils;
         Objects.requireNonNull(config);
         colorMapping.put(PingType.TARGET, config::targetColor);
         Objects.requireNonNull(config);
@@ -168,12 +171,8 @@ public class SocketPingOverlay extends Overlay {
 
     public Dimension render(Graphics2D graphics) {
         Point mousePosition = client.getMouseCanvasPosition();
-        if (isHotkeyPressed || (keyheld != null && (config
-
-                .hotkeyOmw().matches(keyheld) || config
-                .hotkeyQuestionMark().matches(keyheld) || config
-                .hotkeyAssistMe().matches(keyheld) || config
-                .hotkeyWarn().matches(keyheld)))) {
+        if (isHotkeyPressed || (keyheld != null && (config.hotkeyOmw().matches(keyheld) || config.hotkeyQuestionMark().matches(keyheld)
+                || config.hotkeyAssistMe().matches(keyheld) || config.hotkeyWarn().matches(keyheld)))) {
             Color color = config.targetColor();
             if (keyheld != null) {
                 if (config.hotkeyOmw().matches(keyheld))
@@ -196,6 +195,15 @@ public class SocketPingOverlay extends Overlay {
                 for (RadialWheelWidget pingWidget : pingWidgets) {
                     pingWidget.setSelected(pingWidget.isContainedIn(mousePosition));
                     pingWidget.updateLocation(lastMousePosition);
+                    Color wheelColor = RADIAL_COLOR;
+                    if (config.raveWheel() == SocketPingConfig.RaveMode.RAVE) {
+                        wheelColor = plugin.raveColor;
+                    } else if (config.raveWheel() == SocketPingConfig.RaveMode.EPILEPSY) {
+                        wheelColor = Color.getHSBColor(new Random().nextFloat(), 1.0F, 1.0F);
+                    } else if (config.raveWheel() == SocketPingConfig.RaveMode.FLOW) {
+                        wheelColor = raveUtils.getColor(100, client.getGameCycle(), false);
+                    }
+                    pingWidget.setColor(wheelColor);
                     pingWidget.draw(graphics);
                 }
         }
@@ -214,20 +222,43 @@ public class SocketPingOverlay extends Overlay {
             Color color = colorMapping.getOrDefault(pingType, () -> Color.black).get();
             ConcurrentHashMap<Object, Integer> objects = highlightedObjects.get(pingType);
             for (Object o : objects.keySet()) {
+                WorldPoint wp = null;
                 if (o instanceof NPC) {
                     highlight((NPC) o, objects.get(o), color);
-                    continue;
-                }
-                if (o instanceof GameObject) {
+                    wp = ((NPC) o).getWorldLocation();
+                } else if (o instanceof GameObject) {
                     highlight((GameObject) o, objects.get(o), color);
-                    continue;
-                }
-                if (o instanceof Player) {
+                    wp = ((GameObject) o).getWorldLocation();
+                } else if (o instanceof Player) {
                     highlight((Player) o, objects.get(o), color);
-                    continue;
-                }
-                if (o instanceof WorldPoint)
+                    wp = ((Player) o).getWorldLocation();
+                } else if (o instanceof WorldPoint) {
                     highlight(graphics, (WorldPoint) o, objects.get(o), color);
+                    wp = (WorldPoint) o;
+                }
+
+                if (config.pingLocationImage() && wp != null) {
+                    LocalPoint lp = LocalPoint.fromWorld(client, wp);
+                    if (lp != null) {
+                        BufferedImage img;
+                        if (pingType.type.equals("Warn")) {
+                            img = WARN_IMAGE;
+                        } else if (pingType.type.equals("OMW")) {
+                            img = OMW_IMAGE;
+                        } else if (pingType.type.equals("Assist Me")) {
+                            img = ASSIST_ME_IMAGE;
+                        } else if (pingType.type.equals("Question Mark")) {
+                            img = QUESTION_MARK_IMAGE;
+                        } else {
+                            img = PING_IMAGE;
+                        }
+
+                        Point base = Perspective.localToCanvas(client, lp, client.getPlane(), 50);
+                        if (base != null) {
+                            graphics.drawImage(img, base.getX() - 20, base.getY() - 10, null);
+                        }
+                    }
+                }
             }
         }
     }
